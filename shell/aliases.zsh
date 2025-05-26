3df9806dc60d4e83c36bf56c266883c505543883
DetroitCheese <runitstheclergy@gmail.com>
2025-05-26 09:10:40 +0530

    Alias fix: override Homebrew proj binary

---
+++ shell/aliases.zsh
@@ -0,0 +1,10 @@
+# Quick nav
+alias proj="cd ~/projects"
+
+# VSCode in current folder
+alias c.="code ."
+
+# Display modes
+alias pokermode="~/toggle-display-mode.sh poker"
+alias savebattery="~/toggle-display-mode.sh battery"
+alias resetpower="~/toggle-display-mode.sh default"
